72a863e4d29746504c38042bf8352df1a898fae1
Marcio Ferreira <marciosferreira@yahoo.com.br>
2021-06-28 23:58:49 -0400

    Update fish_analyzer_2.py

--- fish_analyzer_2.py
+++ fish_analyzer_2.py
@@ -1,3 +1,6 @@
+# SOFTWARE DESENVOLDIO COM AMPARO DA FUNDAÇÃO DE AMPARO Á PESQUISA DO ESTADO DO AMAZONAS - FAPEAM
+
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
